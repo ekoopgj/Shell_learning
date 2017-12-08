6943b944d64c78423165e366dd4b2eb5090541a3
cheng haung <574637816@qq.com>
2017-12-08 21:25:18 +0900

    Update op.sh

--- op.sh
+++ op.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 i=1
 while [ 0 ];do
-read -p "Enter your MO or attribute name: " name
-if [ $name = "q" ];then
-i=0
-continue
-else
-grep -q $name *.txt
-if [ $? -eq 0 ];then
-    echo "Send to file 2"
-    echo $name >> 2.txt
-else
-    echo "Send to file 1"
-    echo $name >> 1.txt
-fi
-fi
+    read -p "Enter your MO or attribute name: " name
+    if [ $name = "q" ];then
+        i=0
+        continue
+    else
+        grep -q $name *.txt
+        if [ $? -eq 0 ];then
+            echo "Send to file 2"
+            echo $name >> 2.txt
+        else
+            echo "Send to file 1"
+            echo $name >> 1.txt
+        fi
+    fi
 done
